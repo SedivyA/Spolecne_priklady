62241aa8fedeae244409dff189b61e44e34c6cc1
Tomáš Šaroun <74513653+Terms4Ever@users.noreply.github.com>
2020-11-28 23:59:25 +0100

    Add files via upload

---
+++ P02/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random nh = new Random();
+            listBox1.Items.Clear();
+            for (int i = 1; i <= 10; i++)
+            {
+                int x = nh.Next(0, 11);
+                listBox1.Items.Add(x);
+            }
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int soucet = 0;
+            foreach (int a in listBox1.SelectedItems)
+            {
+                soucet += a;
+            }
+            label1.Text = "Počet: " + listBox1.SelectedItems.Count.ToString();
+            label2.Text = "Součet: " + soucet.ToString();
+        }
+    }
+}
---
+++ P04/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int Mocnina(int x, int ep)
+        {
+            int mocnina = 1;
+            for (int i = 0; i < ep; i++)
+            {
+                mocnina *= x;
+            }
+            return mocnina;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int ep = Convert.ToInt32(comboBox1.Text);
+            foreach  (Control con in panel1.Controls)
+            {
+                if (con is TextBox)
+                {
+                    int x = Convert.ToInt32((con as TextBox).Text);
+                    (con as TextBox).Text = Mocnina(x, ep).ToString();
+                }
+                if (con is Label) (con as Label).Text = ep.ToString();
+            }
+
+        }
+    }
+}
